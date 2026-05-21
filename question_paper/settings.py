8c6eba73dd7d2d89f0c608536a7fc4af36bdd300
Shikhar <shikharbari@gmail.com>
2026-05-21 12:17:54 +0530

    add postgresql

--- question_paper/settings.py
+++ question_paper/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_DIR = os.path.join(BASE_DIR, "static")
@@ -56,10 +57,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'question_paper.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL', f'sqlite:///{BASE_DIR}/db.sqlite3'),
+        conn_max_age=600
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
